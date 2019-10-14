62f75b7f1ddf49f5f7b81080f7a02dc169b5313c
nicoletownley <natownley@gmail.com>
2019-10-14 00:31:42 -0500

    coding before handlebars and debugging complete

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "Edith191$1@",
+    database: "burgers_db"
+})
+connection.connect(function(err){
+    if(err)throw err;
+    console.log ("connected as id: "+ connection.threadId)
+})
+module.exports=connection;
--- server.js
+++ server.js
@@ -4,16 +4,21 @@ var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
 var app = express();
-app.use(express.static(_dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
-extended: false}));
+extended: false
+}))
 
 app.use(methodOverride('_method'));
 app.engine("handlebars", exphbs({
 defaultLayout:"main"
 }));
-app.set ("view engine", "handlebars");
+app.set("view engine", "handlebars");
+
+
+var routes = require("./controllers/routes.js");
+app.use('/',routes);
 
 var port = 3000;
-app.listen(port);
+app.listen(port);git
